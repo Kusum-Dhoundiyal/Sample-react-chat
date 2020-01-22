db590aa50537def780ce575e0a6219da8d718ee9
Kusum Dhoundiyal <kusum.dhoundiyal96@gmail.com>
2020-01-22 11:16:13 +0530

    change applicationId

--- src/chat.js
+++ src/chat.js
@@ -5,7 +5,7 @@ class KommunicateChat extends Component {
   }
   componentDidMount(){
     (function(d, m){
-      var kommunicateSettings = {"appId":"kommunicate-support","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var kommunicateSettings = {"appId":"39e72e2fef86f958f8c18ba5d4aa01883","popupWidget":true,"automaticChatOpenOnNavigation":true};
       var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
       s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
       var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
